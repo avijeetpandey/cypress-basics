cde554ae7cd5cf77100ec7edeac8fb9988925942
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-11-30 01:29:44 +0530

    git status add tests to render image and text correctly

---
+++ README.md
@@ -0,0 +1,5 @@
+## Cypress basics :tada: :rocket:
+
+## Notes
+
+- All cypress spec files should end with `.cy.js | .cy.ts` in order for cypress to know that these are spec files
--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="Cypress" />
+
+describe("tasks page", () => {
+  it("should render the image properly", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get(".main-header img").should("be.visible");
+  });
+
+  it("should render the main title correctly", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("h1").contains("React Tasks");
+  });
+});
